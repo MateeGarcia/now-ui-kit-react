87525370e66a39e2a8ba5f4bba8c8d5d66f311e3
Mateo Garcia <51801253+MateeGarcia@users.noreply.github.com>
2024-06-14 18:56:57 -0300

    Agregado componente EstadoDenuncia

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -3,18 +3,17 @@ import React from "react";
 
 // reactstrap components
 import { Col, Row, Container } from "reactstrap";
-import SignUp from "views/index-sections/SignUp";
+import EstadoDenuncia from "views/index-sections/EstadoDenuncia";
 // core components
 
-
-
+import '../../assets/css/IndexHeader.css'
 
 function IndexHeader() {
 
   return (
     <>
       <div className="page-header clear-filter" filter-color="blue">
-        <Container
+        <Container className="main-container-header"
             // style={{
             // backgroundColor: 'rgba(0, 123, 255, 0.5)', // Fondo azul semi-transparente
             // border: '2px solid #007bff', // Borde azul
@@ -32,9 +31,21 @@ function IndexHeader() {
           </Row> 
 
           <Row>
-            <Col style={{ height: '10vh' }}></Col>
+            <Col className="columna-texto">
+            <p> 
+            Estimados:
+            En busca de velar por la integridad y potenciar nuestros valores Mowi Chile, hemos creado este sistema orientado a promover el correcto accionar de todos nuestros colaboradores siguiendo los lineamientos de nuestros valores éticos.
+            </p>
+            <p>
+            El objetivo de esta plataforma es poner en conocimiento, de forma anónima, las conductas que podrían estar fuera del marco legal y reglamentario de acuerdo a nuestro Código de Conducta Mowi Chile. Garantizando la confidencialidad de la información, la instauración de procedimientos para el control y verificación de las denuncias recibidas y por ende, la implementación de los mecanismos necesarios y pertinentes para darles solución de manera óptima.
+            </p>
+            <p> 
+            Invitamos a todos los colaboradores a hacer uso responsable de esta plataforma tecnológica y así,  promover los valores organizacionales y el desarrollo de un grato clima laboral.
+            </p>
+            
+            </Col>
             <Col>
-              <SignUp/>
+              <EstadoDenuncia/>
             </Col>
           </Row>
         </Container>
